c5af8c223161029ed0d717b3ea5e927c85317f6e
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-03-25 14:33:21 -0300

    finish

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { React} from 'react';
+import { StyleSheet, Text, Image, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bem-vindo ao app da Gossip Girls</Text>
+      <Text style={styles.description}>Welcome to the Gossip Girls app</Text>
+      <Image style={styles.image} source={require('./assets/img/gossipgirl.jpg')} />
     </View>
   );
 }
@@ -16,5 +17,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  description: {
+    marginTop: 12,
+    fontSize: 20,
+    color: '#34495e',
+  },
+  image: {
+    justifyContent: 'center',
+    margin:20,
+    width: 300,
+    height: 400,
   },
 });
